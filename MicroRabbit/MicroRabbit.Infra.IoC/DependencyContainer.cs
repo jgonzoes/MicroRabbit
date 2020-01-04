45ea478469007db12afd62411843dd2368970c79
Joaquin Gonzalez <jgonzoes@gmail.com>
2020-01-04 16:16:32 -0500

    Implementacion de IoC

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegistarServices(IServiceCollection services)
+        {
+            //Domanin Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+        }
+    }
+}
